dd64e287bc777f09e8d981d5b4b02de4482c34fe
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-04-20 23:10:29 +0200

    Makefile updated, not functional yet

--- Makefile
+++ Makefile
@@ -1,4 +1,22 @@
-all:
-	gcc -c src/main.c -o target/obj/main.o
-	gcc -c src/stalk.c -o target/obj/stalk.o
-	gcc -o target/bin/stalk target/obj/main.o target/obj/stalk.o
+CC = gcc
+SOURCE_DIR = src/
+TARGET = target/
+TARGET_OBJ = target/obj/
+TARGET_BIN = target/obj/
+OBJS = $(wildcard $(SOURCE_DIR
+
+all: dirs main
+
+clean:
+	rm -rf $(TARGET)
+
+dirs: $(TARGET) $(TARGET_OBJ) $(TARGET_BIN)
+
+%/:
+	mkdir $@
+
+main: $(wildcard $(SOURCE_DIR)*.c:.c=.o)
+	$(CC) -o $(TARGET_BIN)stalk $(TARGET_OBJ)*.o
+
+%.o: %.c
+	$(CC) -o $(TARGET_OBJ)$@ -c $(SOURCE_DIR)$?
--- src/stalk.c
+++ src/stalk.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <errno.h>
+
+static void print_error(const char *filename);
 
 void stalk(const char *filename) {
     struct stat statbuf;
-    time_t last_time = 0;
+
+    int stat_error = stat(filename, &statbuf);
+    time_t last_time = statbuf.st_mtim.tv_sec;
+
+    if (stat_error && errno == ENOENT) {
+        print_error(filename);
+        return;
+    }
+    printf("new");
+    if (!stat_error) {
+        printf("Currently stalking %s\n", filename);
+    }
 
     while (1) {
-        int stat_error = stat(filename, &statbuf);
-       
-        if (stat_error) {
-            fprintf(stderr, "Error checking file %s: ", filename);
-            perror("");
+        stat_error = stat(filename, &statbuf);
+
+        if (stat_error && errno != ENOENT) {
+            print_error(filename);
         }
         
         time_t new_time = statbuf.st_mtim.tv_sec;
@@ -20,5 +33,10 @@ void stalk(const char *filename) {
         last_time = new_time;
         printf("File last changed at %ld\n", last_time);
     }
+}
+
+static void print_error(const char *filename) {
+    fprintf(stderr, "Error checking file %s: ", filename);
+    perror("");
 }
 
