5ccd1d7f73053f52a795d52cf4e5b3308d304550
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-04-21 23:11:40 +0200

    Time printing improved

--- src/stalk.c
+++ src/stalk.c
@@ -4,18 +4,19 @@
 #include <time.h>
 
 static void print_error(const char *filename);
-static void print_time(FILE *stream, time_t *time);
+static int print_time(FILE *stream, time_t *time);
 
 void stalk(const char *filename) {
     struct stat statbuf;
 
     int stat_error = stat(filename, &statbuf);
-    time_t last_time = statbuf.st_mtim.tv_sec;
 
-    if (stat_error && errno == ENOENT) {
+    if (stat_error) {
         print_error(filename);
         return;
     }
+
+    long last_time = statbuf.st_mtim.tv_nsec;
     if (!stat_error) {
         printf("Currently stalking %s\n", filename);
     }
@@ -27,13 +28,14 @@ void stalk(const char *filename) {
             print_error(filename);
         }
         
-        time_t new_time = statbuf.st_mtim.tv_sec;
+        long new_time = statbuf.st_mtim.tv_nsec;
         if (new_time == last_time) {
             continue;
         }
         last_time = new_time;
-        printf("File changed!\nTime: %ld\nFormatted time: ", last_time);
+        printf("[");
         print_time(stdout, &statbuf.st_mtim.tv_sec);
+        printf("] File changed!\n");
     }
 }
 
@@ -42,10 +44,19 @@ static void print_error(const char *filename) {
     perror("");
 }
 
-static void print_time(FILE *stream, time_t *time) {
-    static const int BUFSIZE = 100;
-    char buf[BUFSIZE];
-    ctime_r(time, buf);
-    fprintf(stream, "%s", buf);
+static int print_time(FILE *stream, time_t *timep) {
+    static struct tm time_breakdown;
+    static struct tm *time_result = NULL;
+    time_result = localtime_r(timep, &time_breakdown);
+
+    if (time_result == NULL) {
+        return -1;
+    }
+    return fprintf(
+            stream,
+            "%02d:%02d:%02d",
+            time_breakdown.tm_hour,
+            time_breakdown.tm_min,
+            time_breakdown.tm_sec);
 }
 
