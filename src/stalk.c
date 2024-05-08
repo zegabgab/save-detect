bb3f29c45cd6ab39badbd0445cf9c4d9a0d41d68
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-05-09 00:40:38 +0200

    we fixed it!

--- src/stalk.c
+++ src/stalk.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/file.h>
-#include <unistd.h>
+#include <sys/inotify.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <time.h>
 
@@ -10,41 +9,20 @@ static void print_error(const char *filename);
 static int print_time(FILE *stream, time_t *time);
 
 int stalk(const char *filename) {
-    struct stat statbuf;
-    int filedesc = open(filename, O_RDONLY);
-    if (filedesc < 0) {
+    int notify = inotify_init();
+    if (notify == -1) {
         return 1;
     }
-    if (flock(filedesc, LOCK_SH)) {
-        close(filedesc);
-        return 2;
-    }
-
-    int stat_error = fstat(filedesc, &statbuf);
-
-    if (stat_error) {
-        print_error(filename);
-        return 1;
-    }
-
-    long last_time = statbuf.st_mtim.tv_nsec;
-
-    while (1) {
-        stat_error = fstat(filedesc, &statbuf);
-
-        if (stat_error) {
-            print_error(filename);
-        }
-        
-        long new_time = statbuf.st_mtim.tv_nsec;
-        if (new_time == last_time) {
-            continue;
-        }
-        last_time = new_time;
-        printf("[");
-        print_time(stdout, &statbuf.st_mtim.tv_sec);
-        printf("] File changed!\n");
-    }
+    int watch = inotify_add_watch(notify, filename, IN_MODIFY);
+    struct inotify_event mod_event;
+    ssize_t bytes = read(notify, &mod_event, sizeof mod_event);
+    time_t now;
+    time(&now);
+    printf("[");
+    print_time(stdout, &now);
+    printf("] File changed!\n");
+    inotify_rm_watch(notify, watch);
+    return close(notify);
 }
 
 static void print_error(const char *filename) {
