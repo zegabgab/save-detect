4c0cd87b077f8e2bc8d5cb20b32fa730d02190f1
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-05-11 19:22:53 +0200

    Small beginnings of multi-platform stalking

--- src/checkfile.c
+++ src/checkfile.c
@@ -1,7 +1,12 @@
-#include <unistd.h>
 #include "checkfile.h"
+
+#ifdef __unix__
+#include <unistd.h>
 
 bool confirmaccessible(const char *pathname) {
     return access(pathname, F_OK);
 }
 
+#else
+#error Platform not supported
+#endif
