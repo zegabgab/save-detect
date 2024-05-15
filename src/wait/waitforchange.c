08271128273dc466456ffae898266ff7c5ad2399
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-05-15 17:39:52 +0200

    Added waiter type to watch files

--- src/wait/waitforchange.c
+++ src/wait/waitforchange.c
@@ -2,18 +2,32 @@
 
 #ifdef __linux__
 
+#include <stdlib.h>
 #include <sys/inotify.h>
-#include "waitforchange.h"
 
-int init_waiter() {
+int init_waiter(struct waiter *waiter) {
+    if (waiter == NULL) {
+        return -1;
+    }
+    waiter->inotify_instance = -1;
+    waiter->watched_files = NULL;
+    waiter->watched_num = 0;
+    return 0;
+}
+
+struct waiter *new_waiter() {
+    struct waiter *result = malloc(sizeof *result);
+    if (!result) {
+        return NULL;
+    }
+    return result;
+}
+
+int waitforchange(struct waiter *waiter, const char *pathname) {
     return -1;
 }
 
-int waitforchange(const char *pathname) {
-    return -1;
-}
-
-int cleanup_waiter() {
+int cleanup_waiter(struct waiter *waiter) {
     return -1;
 }
 
--- src/wait/waitforchange.h
+++ src/wait/waitforchange.h
@@ -1,10 +1,24 @@
 #ifndef _WAITFORCHANGE_H
 #define _WAITFORCHANGE_H
 
-int init_waiter();
+struct waiter;
 
-int waitforchange(const char *pathname);
+#ifdef __linux__
 
-int cleanup_waiter();
+struct waiter {
+    int inotify_instance;
+    int *watched_files;
+    int watched_num;
+};
+
+#endif // linux waiter type
+
+int init_waiter(struct waiter *waiter);
+
+struct waiter *new_waiter();
+
+int waitforchange(struct waiter *waiter, const char *pathname);
+
+int cleanup_waiter(struct waiter *waiter);
 
 #endif
